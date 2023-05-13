f129b3747c16098eb4cd149f5e3244819d88608f
Luis Eduardo <luis.cg_github@hotmail.com>
2023-05-13 16:07:33 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Ola Mundo");
+		
+		Board board = new Board(8, 8);
 
 	}
 
